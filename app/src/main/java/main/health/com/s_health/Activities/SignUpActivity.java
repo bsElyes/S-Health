aa98db8508520bbf3707235bc824cdabc34458be
Elyes <bselyes@gmail.com>
2015-11-11 06:14:12 +0100

    Auth

---
+++ app/src/main/java/main/health/com/s_health/Activities/SignUpActivity.java
@@ -0,0 +1,17 @@
+package main.health.com.s_health.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import main.health.com.s_health.R;
+
+public class SignUpActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+
+
+    }
+}
